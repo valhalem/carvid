443e5f487e8e262afb0d231fc20304407dafac8e
Konrad Stachowicz <konrad.stachowicz@firma.o2.pl>
2014-03-26 13:47:48 +0100

    test linkowania

--- carvid/article/views.py
+++ carvid/article/views.py
@@ -4,24 +4,21 @@ from django.http import HttpResponse, Http404
 from article.models import Article
 
 def index(request):
-	articles = Article.objects.all().order_by('-pub_date')
-	t = loader.get_template('index.html')
-	c = Context ({'articles': articles,})
-	return HttpResponse(t.render(c))
+    articles = Article.objects.all().order_by('-pub_date')
+    return render_to_response( 'index.html', { 'articles' : articles })
 
 def article_details(request, article):
-	idk = Article.objects.get(pk=article)
-	td = loader.get_template('details.html')
-	cd = Context({
-		'article_title': idk ,
-		'article_video_url': idk.video_url,
-        'article_tags' : idk.tags.all(),
-        'article_lead': idk.lead ,
-        'article_votes': idk.votes,
-        'nickname' : idk.nickname ,
-        'city' : idk.city ,
-        })
-	return HttpResponse(td.render(cd))
+    idk = Article.objects.get(pk=article)
+
+    return render_to_response( 'details.html',
+            {
+                'article_title': idk ,
+                'article_video_url': idk.video_url,
+                'article_tags' : idk.tags.all(),
+                'article_lead': idk.lead ,
+                'article_votes': idk.votes,
+                'nickname' : idk.nickname ,
+                'city' : idk.city , })
 
 def tags( request ):
     articles_by_tag = Article.objects.all()
@@ -34,6 +31,8 @@ def tags_search(request, tag):
     except IndexError:
         raise Http404
     return render_to_response( 'tag_list.html',
-            { 'list': by_tag,
-              'title': by_tag.video_url ,
-              'lead': by_tag.pub_date, })
+            { 
+              'list': by_tag ,
+              'title': by_tag.id ,
+              'lead': by_tag.pub_date, 
+              })
