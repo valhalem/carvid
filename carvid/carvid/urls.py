045d29dfb0f2b8da4c4796095fc47c4a23eea2b3
Konrad Stachowicz <konrad.stachowicz@firma.o2.pl>
2014-03-26 15:00:14 +0100

    index

--- carvid/carvid/urls.py
+++ carvid/carvid/urls.py
@@ -6,7 +6,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'carvid.views.home', name='home'),
+    url(r'^$', 'article.views.index',),
     # url(r'^carvid/', include('carvid.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
