7b05f59c5d7c249596ed690688f290174109e22d
Konrad Stachowicz <konrad.stachowicz@firma.o2.pl>
2014-03-26 15:48:14 +0100

    sciezka templates

--- carvid/carvid/settings.py
+++ carvid/carvid/settings.py
@@ -113,9 +113,9 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    "/home/kstachowicz/Documents/carvid/templates/"
+    os.getcwd() + "/templates/"
 )
-
+print TEMPLATE_DIRS
 INSTALLED_APPS = (
     'django.contrib.auth',
     'django.contrib.contenttypes',
